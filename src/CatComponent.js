e6c03b0bc290ece057556ac89feed3faafd8e784
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-15 13:12:29 +0000

    Automatically backed up by Learn

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class CatComponent extends Component {
+class CatComponent extends React.Component {
   render() {
     return (
       <div className="bar" id="cat">
